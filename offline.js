744022a201e71c6a89494d7beb950cb7a1cd78dd
benjazor <benjazor@gmail.com>
2018-12-07 06:09:27 +0100

    upload arcade game

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1544159275,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/default-sheet0.png",
+		"images/default2-sheet0.png",
+		"images/default3-sheet0.png",
+		"images/player-sheet0.png",
+		"images/background.png",
+		"images/bullet-sheet0.png",
+		"images/gamelogo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
